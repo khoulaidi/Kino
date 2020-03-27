1e4ea8c6ba51b30110ed739902d39d8a1e3f8640
abaffoun <40427470+abaffoun@users.noreply.github.com>
2020-03-27 23:53:34 +0100

    Migration

---
+++ Startseite.php
@@ -0,0 +1,402 @@
+<!doctype html>
+<html lang="en">
+	<head>
+  
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="homebg.css">
+
+	<title >startseite</title>
+
+	<!-- Favicons -->
+	<link rel="apple-touch-icon" href="/docs/4.4/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+	<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">		
+	<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+	<link rel="manifest" href="/docs/4.4/assets/img/favicons/manifest.json">
+	<link rel="mask-icon" href="/docs/4.4/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+	<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon.ico">
+	<meta name="msapplication-config" content="/docs/4.4/assets/img/favicons/browserconfig.xml">
+	<meta name="theme-color" content="#563d7c">
+
+
+		<style>
+
+		.form-popup {
+  display: none;
+  background-color:#D3D3D3;
+  border: 3px solid background-color:#D3D3D3;
+  z-index: 9;
+}
+
+/* Add styles to the form container */
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
+
+
+/* Set a style for the submit/login button */
+.form-container .btn {
+  background-color: #4CAF50;
+  color: blau;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom:10px;
+  opacity: 0.8;
+}
+
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: red;
+}
+
+/* Add some hover effects to buttons */
+.form-container .btn:hover, .open-button:hover {
+  opacity: 1;
+}
+		
+		</style>
+		<!-- Custom styles for this template -->
+		<link href="album.css" rel="stylesheet">
+	</head>
+  
+	
+    <header>
+		<div class="collapse bg-dark" id="navbarHeader">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 col-md-7 py-4">
+						<h4 class="text-white">About</h4>
+						<p class="text-muted"> Diese webseite ist ein exslusive Projekt für Internet Technologie.Das ProjektTeam bestehst aus 2 Studenten (Ahmed Baffoun & Ibrahim ....).</p>
+						<p class="text-muted">in Unsere Webseit kann der kinoBesucher ein Ticket für film buchen sowie auch platz Reservieren. </p>
+					</div>
+					<div class="col-sm-4 offset-md-1 py-4">
+						<h4 class="text-white">Contact</h4>
+						<ul class="list-unstyled">
+							<li><a  class="text-white">Unser Servicecenter ist in der aktuellen Lage nur per Email erreichbar</a></li>
+							<li><a  class="text-white">diese Email kontaktieren:</a></li>
+							<li><a class="text-white">ahmed_baffana@hotmail.com</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="navbar navbar-dark bg-dark shadow-sm">
+			<div class="container d-flex justify-content-between">
+				<a href="file:///C:/Users/ahmed/Desktop/Kino/Startseite.html" class="navbar-brand d-flex align-items-center" style="color:#F6D155">
+					<strong>Kinoprogramm</strong>
+				</a>
+				<a id="anmelden" href="file:///C:/Users/ahmed/Desktop/Kino/Anmeldung.html" class="navbar-brand d-flex align-items-center"style="color:#F6D155">
+					<strong>Anmelden</strong>
+				</a>
+				<a href="file:///C:/Users/ahmed/Desktop/Kino/Regestrieren.html" class="navbar-brand d-flex align-items-center"style="color:#F6D155">
+					<strong>Registrieren</strong>
+				</a>
+				<button class="navbar-toggler" style="color:#F6D155" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span> Info
+				</button>
+			</div>
+		</div>
+	</header>
+	<main style="background-color: #1d1e22 "  role="main">
+		<div  class="container">
+			<div id="demo" class="carousel slide" data-ride="carousel">
+
+				<!-- Indicators -->
+				<ul class="carousel-indicators">
+					<li data-target="#demo" data-slide-to="0" class="active"></li>
+					<li data-target="#demo" data-slide-to="1"></li>
+					<li data-target="#demo" data-slide-to="2"></li>
+					<li data-target="#demo" data-slide-to="3"></li>
+				</ul>
+				<!-- The slideshow -->
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<div class="info-container">
+							<img src="https://images.fandango.com/ImageRenderer/0/0/redesign/static/img/default_poster.png/0/images/masterrepository/Fandango/219414/JM2_OnLine_1080x1350_TSR_HERO_RD3DDCIMX_01.jpg" alt="Los Angeles" width="1100" height="500">
+								<div style="color:#B1A296" class="slide-title h2 col-xs-24">Demnächst verfügbar</div>
+						</div>
+					</div>
+	
+					<div class="carousel-item">
+						<img src="https://m.media-amazon.com/images/M/MV5BMjQ2ODIyMjY4MF5BMl5BanBnXkFtZTgwNzY4ODI2NzM@._V1_.jpg" alt="Chicago" width="1100" height="500">
+							<div style="color:#B1A296"class="slide-title h2 col-xs-24">Demnächst verfügbar</div>
+					</div>
+    
+					<div class="carousel-item">
+						<img src="https://upload.wikimedia.org/wikipedia/en/thumb/2/29/Movie_poster_for_%22Scary_Movie%22.jpg/220px-Movie_poster_for_%22Scary_Movie%22.jpg" alt="Chicago" width="1100" height="500">
+							<div style="color:#B1A296"class="slide-title h2 col-xs-24">Demnächst verfügbar</div>
+					</div>
+	
+					<div class="carousel-item">
+						<img src="https://cdn.eventcinemas.co.nz/cdn/resources/movies/14819/images/largeposter.jpg" alt="Chicago" width="1100" height="500">
+							<div style="color:#B1A296"class="slide-title h2 col-xs-24">Demnächst verfügbar</div>
+					</div>
+
+				</div>
+  
+				<!-- Left and right controls -->
+				<a class="carousel-control-prev" href="#demo" data-slide="prev">
+					<span class="carousel-control-prev-icon"></span>
+				</a>
+				<a class="carousel-control-next" href="#demo" data-slide="next">
+					<span class="carousel-control-next-icon"></span>
+				</a>
+			</div>
+     
+ 
+		
+		<div class="album py-5 bg-light"   >
+			<div class="container">
+				<div class="row">	
+					<div class="col-md-4">
+						<div id="morbius"class="card mb-4 shadow-sm">
+							<img src="https://cdn.cinemaxx.de/-/media/images/cinemaxx/cinemaxxposter/26001-172388poster1-posterjpg.jpg?h=477&iar=1&w=319" alt="Chicago">
+							<div class="card-body"style="background-color:#D3D3D3">
+								<p class="card-text"style="font-family:Roboto, sans-serif"><i>In MORBIUS verkörpert Jared Leto eindrucksvoll eine der geheimnisvollsten Figuren im Marvel-Universum.</i></p>
+								<p class="card-text" style="font-family:arial"> <b>Genre:</b> Action.</p>
+								<p class="card-text"> <b>Filmstart:</b> 06 August 2020.</p>
+								<p class="card-text"> <b>Dauer:</b> 96 mn.</p>
+								<div class="d-flex justify-content-between align-items-center">
+									<div class="btn-group">
+									
+	<button id="hotelres"type="button" class="btn btn-sm btn-outline-secondary"onclick="openForm1()">Reservieren</button>
+</div>   
+
+
+								</div>
+								<div class="form-popup" id="RF1">
+
+
+								<button type="button" class="btn btn-secondary" style onclick=>22.04.20  18 uhr</button><br>
+								<button type="button" class="btn btn-secondary" onclick=>22.04.20  22 uhr</button><br>
+								<button type="button" class="btn btn-secondary"onclick=>23.04.20  20 uhr</button><br>
+								<button type="button" class="btn cancel" onclick="closeForm1()" style="background-color:red">zurück</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+		
+					<div id="peterhase2"class="col-md-4">
+						<div class="card mb-4 shadow-sm">
+							<img src="https://cdn.cinemaxx.de/-/media/images/cinemaxx/cinemaxxposter/26493-170129poster4-posterjpg.jpg?h=477&iar=1&w=319" alt="Chicago">
+							<div class="card-body" style="background-color:#D3D3D3">
+								<p class="card-text" style="font-family:Roboto, sans-serif" > <i>Zweites aufregendes Kinoabenteuer für Peter Hase mit Domhnall Gleeson und Rose Byrne.</i></p>
+								<p class="card-text" style="font-family:arial"> <b>Genre:</b> Komödie.</p>
+								<p class="card-text"> <b>Filmstart:</b> 30 juli 2020.</p>
+								<p class="card-text"> <b>Dauer:</b> 80 mn.</p>
+								<div class="d-flex justify-content-between align-items-center">
+																		<div class="btn-group">
+									
+	<button id="hotelres"type="button" class="btn btn-sm btn-outline-secondary"onclick="openForm2()">Reservieren</button>
+</div>   
+
+
+								</div>
+								<div class="form-popup" id="RF2">
+
+
+								<button type="button" class="btn btn-secondary" style onclick=>22.04.20  18 uhr</button><br>
+								<button type="button" class="btn btn-secondary" onclick=>22.04.20  22 uhr</button><br>
+								<button type="button" class="btn btn-secondary"onclick=>23.04.20  20 uhr</button><br>
+								<button type="button" class="btn cancel" onclick="closeForm2()" style="background-color:red">zurück</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div id="joker"class="col-md-4">
+						<div class="card mb-4 shadow-sm">
+							<img src="https://cdn.cinemaxx.de/-/media/images/cinemaxx/cinemaxxposter/25683-170748poster2-posterjpg.jpg"style="height: 477px;"alt="joki">
+							<div class="card-body" style="background-color:#D3D3D3">
+								<p class="card-text" style="font-family:Roboto, sans-serif" > <i>1981 in Gotham City: Arthur Fleck hat sich geirrt. Sein Dasein ist keine Komödie, sondern eine Tragödie...</i></p>
+								<p class="card-text" style="font-family:arial"> <b>Genre:</b>Krimi, Action</p>
+								<p class="card-text"> <b>Filmstart:</b> 10 Oktober 2019.</p>
+								<p class="card-text"> <b>Dauer:</b> 122 mn.</p>
+								<div class="d-flex justify-content-between align-items-center">
+																		<div class="btn-group">
+									
+	<button id="hotelres"type="button" class="btn btn-sm btn-outline-secondary"onclick="openForm3()">Reservieren</button>
+</div>   
+
+
+								</div>
+								<div class="form-popup" id="RF3">
+
+
+								<button type="button" class="btn btn-secondary" style onclick=>22.04.20  18 uhr</button><br>
+								<button type="button" class="btn btn-secondary" onclick=>22.04.20  22 uhr</button><br>
+								<button type="button" class="btn btn-secondary"onclick=>23.04.20  20 uhr</button><br>
+								<button type="button" class="btn cancel" onclick="closeForm3()" style="background-color:red">zurück</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>		
+
+					<div id="narziss"class="col-md-4">
+						<div class="card mb-4 shadow-sm">
+							<img src="https://cdn.cinemaxx.de/-/media/images/cinemaxx/cinemaxxposter/25298-143991poster3-posterjpg.jpg"style="height: 477px;"alt="joki">
+							<div class="card-body" style="background-color:#D3D3D3" >
+								<p class="card-text" style="font-family:Roboto, sans-serif"><i>Film von Hermann gleichnamigem Roman über die Freundschaft zwischen zwei gegensätzlichen Männern.</i></p>
+								<p class="card-text" style="font-family:arial"> <b>Genre:</b>Drama</p>
+								<p class="card-text"> <b>Filmstart:</b> 12 März 2020.</p>
+								<p class="card-text"> <b>Dauer:</b> 118 mn.</p>
+								<div class="d-flex justify-content-between align-items-center">
+																		<div class="btn-group">
+									
+	<button id="hotelres"type="button" class="btn btn-sm btn-outline-secondary"onclick="openForm4()">Reservieren</button>
+</div>   
+
+
+								</div>
+								<div class="form-popup" id="RF4">
+
+
+								<button type="button" class="btn btn-secondary" style onclick=>22.04.20  18 uhr</button><br>
+								<button type="button" class="btn btn-secondary" onclick=>22.04.20  22 uhr</button><br>
+								<button type="button" class="btn btn-secondary"onclick=>23.04.20  20 uhr</button><br>
+								<button type="button" class="btn cancel" onclick="closeForm4()" style="background-color:red">zurück</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div id="nightlife"class="col-md-4">
+						<div class="card mb-4 shadow-sm">
+							<img src="https://cdn.cinemaxx.de/-/media/images/cinemaxx/cinemaxxposter/26162-172565poster3-posterjpg.jpg"style="height: 477px;"alt="joki">
+							<div class="card-body" style="background-color:#D3D3D3" >
+								<p class="card-text" style="font-family:Roboto, sans-serif"><i>Regisseur Simon Verhoeven schickt Elyas M'Barek und Palina Rojinski auf das verrückteste Date aller Zeiten.</i></p>
+								<p class="card-text" style="font-family:arial"> <b>Genre:</b>Komödie</p>
+								<p class="card-text"> <b>Filmstart:</b> 13 Februar  2020.</p>
+								<p class="card-text"> <b>Dauer:</b> 115 mn.</p>
+								<div class="d-flex justify-content-between align-items-center">
+									<div class="btn-group">
+									
+	<button id="hotelres"type="button" class="btn btn-sm btn-outline-secondary"onclick="openForm5()">Reservieren</button>
+</div>   
+
+
+								</div>
+								<div class="form-popup" id="RF5">
+
+
+								<button type="button" class="btn btn-secondary" style onclick=>22.04.20  18 uhr</button><br>
+								<button type="button" class="btn btn-secondary" onclick=>22.04.20  22 uhr</button><br>
+								<button type="button" class="btn btn-secondary"onclick=>23.04.20  20 uhr</button><br>
+								<button type="button" class="btn cancel" onclick="closeForm5()" style="background-color:red">zurück</button>
+									</form>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+       
+					<div id="hotel"class="col-md-4">
+						<div class="card mb-4 shadow-sm">
+							<img src="https://cdn.cinemaxx.de/-/media/images/cinemaxx/cinemaxxposter/27095-100901658cxxposter1-posterjpg.jpg"style="height: 477px;"alt="joki">
+							<div class="card-body" style="background-color:#D3D3D3" >
+								<p class="card-text" style="font-family:Roboto, sans-serif"><i>Die serbische Romantik-Komödie HOTEL BELGRAD im CinemaxX auf der großen Leinwand.</i></p>
+								<p class="card-text" style="font-family:arial"> <b>Genre:</b>Event</p>
+								<p class="card-text"> <b>Filmstart:</b> 19 März  2020.</p>
+								<p class="card-text"> <b>Dauer:</b> 106 mn.</p>
+								<div class="d-flex justify-content-between align-items-center">
+<div class="btn-group">
+									
+	<button id="hotelres"type="button" class="btn btn-sm btn-outline-secondary"onclick="openForm()">Reservieren</button>
+</div>   
+
+
+								</div>
+								<div class="form-popup" id="myForm">
+
+
+								<button type="button" class="btn btn-secondary" style onclick=>22.04.20  18 uhr</button><br>
+								<button type="button" class="btn btn-secondary" onclick=>22.04.20  22 uhr</button><br>
+								<button type="button" class="btn btn-secondary"onclick=>23.04.20  20 uhr</button><br>
+								<button type="button" class="btn cancel" onclick="closeForm()" style="background-color:red">zurück</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					
+				</div>
+			</div>
+		</div>
+		
+
+<script>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+function openForm1() {
+  document.getElementById("RF1").style.display = "block";
+}
+
+function closeForm1() {
+  document.getElementById("RF1").style.display = "none";
+}
+function openForm2() {
+  document.getElementById("RF2").style.display = "block";
+}
+
+function closeForm2() {
+  document.getElementById("RF2").style.display = "none";
+}
+function openForm3() {
+  document.getElementById("RF3").style.display = "block";
+}
+
+function closeForm3() {
+  document.getElementById("RF3").style.display = "none";
+}
+function openForm4() {
+  document.getElementById("RF4").style.display = "block";
+}
+
+function closeForm4() {
+  document.getElementById("RF4").style.display = "none";
+}
+function openForm5() {
+  document.getElementById("RF5").style.display = "block";
+}
+
+function closeForm5() {
+  document.getElementById("RF5").style.display = "none";
+}
+</script>
+	</main>
+<footer class="text-muted" style="background-color:#212529">
+	<div class="container"style="background-color:#212529">
+		<p class="float-right">
+		
+			<a href="#">Back to top</a>
+			<br>#diese Webseite steht immer noch in test phase (Beta)
+		</p>
+		<p>Diese webseit &copy; Ahmed Baffoun</p>
+		<p>*diese Seite ist nur in Deutschland erreichbar</P>
+	</div>
+</footer>
+</div>
+<body style="background-color:#212529">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ Ticket.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+	<head>
+  
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="homebg.css">
+
+	<title >Ticket</title>
+	<!-- Favicons -->
+	<link rel="apple-touch-icon" href="/docs/4.4/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+	<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">		
+	<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+	<link rel="manifest" href="/docs/4.4/assets/img/favicons/manifest.json">
+	<link rel="mask-icon" href="/docs/4.4/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+	<link rel="icon" href="/docs/4.4/assets/img/favicons/favicon.ico">
+	<meta name="msapplication-config" content="/docs/4.4/assets/img/favicons/browserconfig.xml">
+	<meta name="theme-color" content="#563d7c">
+
+		<!-- Custom styles for this template -->
+		<link href="album.css" rel="stylesheet">
+	</head>
+  
+	
+    <header>
+		<div class="collapse bg-dark" id="navbarHeader">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 col-md-7 py-4">
+						<h4 class="text-white">About</h4>
+						<p class="text-muted"> Diese webseite ist ein exslusive Projekt für Internet Technologie.Das ProjektTeam bestehst aus 2 Studenten (Ahmed Baffoun & Ibrahim ....).</p>
+						<p class="text-muted">in Unsere Webseit kann der kinoBesucher ein Ticket für film buchen sowie auch platz Reservieren. </p>
+					</div>
+					<div class="col-sm-4 offset-md-1 py-4">
+						<h4 class="text-white">Contact</h4>
+						<ul class="list-unstyled">
+							<li><a  class="text-white">Unser Servicecenter ist in der aktuellen Lage nur per Email erreichbar</a></li>
+							<li><a  class="text-white">diese Email kontaktieren:</a></li>
+							<li><a class="text-white">ahmed_baffana@hotmail.com</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="navbar navbar-dark bg-dark shadow-sm">
+			<div class="container d-flex justify-content-between">
+				<a href="file:///C:/Users/ahmed/Desktop/Startseite.html" class="navbar-brand d-flex align-items-center" style="color:#F6D155">
+					<strong>Kinoprogramm</strong>
+				</a>
+				<a id="anmelden" href="file:///C:/Users/ahmed/Desktop/Kino/Anmeldung.html" class="navbar-brand d-flex align-items-center"style="color:#F6D155">
+					<strong>Anmelden</strong>
+				</a>
+				<a href="file:///C:/Users/ahmed/Desktop/Kino/Regestrieren.html" class="navbar-brand d-flex align-items-center"style="color:#F6D155">
+					<strong>Registrieren</strong>
+				</a>
+				<button class="navbar-toggler" style="color:#F6D155" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span> Info
+				</button>
+			</div>
+		</div>
+	</header>
+<main class="text-center" style="background-color: #1d1e22 ">
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+  <center><div class="card"  style="width:500px">
+			<img class="card-img-top" src="ticket.jpg" alt="Card image">
+			<div class="card-img-overlay">
+				<div class="container" style="
+					height: 200px;
+					width: 280px;
+					padding-left: 55px;
+					
+					padding-top: 16px;">
+					
+  <!-- hier werden die info vom daten bank gerufen .sie solten in der richtige reihnfolgen geschrieben werden wie im ticket steht-->		</div>
+
+			</div>
+	</div></center>	
+<center> <h2 style="color:#D2C29D"> Reservation bestätigt </h2> </center>
+<center><p style="color:#D2C29D">Vielen Dank & Viel spaß</p><center>
+<center><p> </p></center>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+</main>
+
+<footer class="text-muted" style="background-color:#212529">
+	<div class="container"style="background-color:#212529">
+		<p class="float-right">
+		
+			<a href="#">Back to top</a>
+			<br>#diese Webseite steht immer noch in test phase (Beta)
+		</p>
+		<p>Diese webseit &copy; Ahmed Baffoun</p>
+		<p>*diese Seite ist nur in Deutschland erreichbar</P>
+	</div>
+</footer>
+<body style="background-color:#212529">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
