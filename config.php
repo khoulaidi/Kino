7c9239ad7a2bf1fc1d77adfdbcec21380b02c59c
khoulaidi <zoubairbair@gmail.com>
2020-03-29 08:39:49 +0200

    gereinigt

--- config.php
+++ config.php
@@ -7,27 +7,4 @@
   $image_path = $host.$file."/images/";
 
   Connection::Connect();
-  /*static function Test(){
-    $sql= "SELECT * FROM FILM WHERE id = 1";
-    $result = self::$con->query($sql);
-
-    if($result->num_rows > 0){
-      $row = $result->fetch_assoc();
-      echo '<img src ="'.$row["image"].'" width="25%" />';
-    }
-  }*/
-
-  /*$joker = new _Film("Night", "Regisseur Simon Verhoeven schickt Elyas MBarek und Palina Rojinski auf das verrueckteste Date aller Zeiten.",
-   "115", "13.02.2020", "Simon Verhoeven", "http://".$image_path."Night.jpg");
-
-   if(Connection::insertFilm($joker)){
-     echo 'Richtig';
-   }
-   else {
-     echo 'Falsch';
-   }
-
-   echo $joker->convertDatum();*/
-
-
  ?>
